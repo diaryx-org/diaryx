eb6d7c2505d66ef14cc49e2b2be42f8341d6b239
Adam Harris <adam@diaryx.org>
2026-02-11 17:15:48 -0700

    chore: bump to v0.12.0

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
             rustc = rustToolchain;
           }).buildRustPackage {
             pname = "diaryx";
-            version = "0.11.1";
+            version = "0.12.0";
             inherit src;
             cargoLock.lockFile = ./Cargo.lock;
             cargoBuildFlags = [ "-p" "diaryx" ];
